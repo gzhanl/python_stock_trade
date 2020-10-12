89fe36f99d64503293a7edfd15967bcefab372c3
gzhanl <gzlianghan@163.com>
2020-10-12 08:12:05 +0800

    20201012_2

---
+++ ch7/7.1.1.py
@@ -0,0 +1,9 @@
+import pandas_datareader.data as web
+
+import datetime
+
+df_stockload= web.DataReader("000001.SS", "yahoo",datetime.datetime(2009,1,1),datetime.datetime(2019,6,1))
+
+print(df_stockload.head())
+
+print(df_stockload.tail())
